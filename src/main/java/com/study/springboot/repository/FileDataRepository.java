47dc2362bc7b395770e3855c338ecda8d4845972
yoon <whyjmlearn@gmail.com>
2024-01-08 18:34:48 +0900

    0108 end

--- src/main/java/com/study/springboot/repository/FileDataRepository.java
+++ src/main/java/com/study/springboot/repository/FileDataRepository.java
@@ -1,16 +1,14 @@
 package com.study.springboot.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.study.springboot.entity.FileData;
 
 @Repository
-public interface FileDataRepository extends JpaRepository<FileData, Long>{
+public interface FileDataRepository extends JpaRepository<FileData, String>{
 
 	List<FileData> findByBoardBno(Long boardBno);
 	List<FileData> findByUuid(String uuid);
