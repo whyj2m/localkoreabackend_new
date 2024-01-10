8b99e27c55eb8e138c7acee9d8b44f7c76f06062
seunghyeon <vjvmf123456@gmail.com>
2024-01-10 18:11:43 +0900

    댓글 api

--- src/main/java/com/study/springboot/repository/BoardReplyRepository.java
+++ src/main/java/com/study/springboot/repository/BoardReplyRepository.java
@@ -1,6 +1,5 @@
 package com.study.springboot.repository;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,8 +10,8 @@ import com.study.springboot.entity.BoardReply;
 @Repository
 public interface BoardReplyRepository extends JpaRepository<BoardReply, Long> {
 
-//	 Optional<BoardReply> findByBno(Long bno);
-	 // 첫 번째 댓글을 생성 시간순으로 찾는 쿼리 메서드
-//	 List<BoardReply> findByBno(Long bno);
+//	Optional<BoardReply> findByBno(Long boardBno);
+//	Optional<BoardReply> findByBno(Long bno);
+	
 }
 
