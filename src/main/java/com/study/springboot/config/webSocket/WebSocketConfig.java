e34add45134a14b0c40d10ad8c421dfcdcb1be8c
yoon <whyjmlearn@gmail.com>
2024-01-23 21:13:52 +0900

    01.23 채팅창구현중( config.webSocket 추가 )

---
+++ src/main/java/com/study/springboot/config/webSocket/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.study.springboot.config.webSocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new WebSocketHandler(), "/socket")
+                .setAllowedOrigins("http://localhost:3000");
+    }
+}
