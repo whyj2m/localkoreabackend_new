71d9e54fd0ca60c4ecad4e87b18309a50c075cf8
100ke <bhy9803@gmail.com>
2024-01-24 21:01:22 +0900

    240124 회원 탈퇴 기능 구현중

--- src/main/java/com/study/springboot/repository/BoardReplyRepository.java
+++ src/main/java/com/study/springboot/repository/BoardReplyRepository.java
@@ -23,4 +23,10 @@ public interface BoardReplyRepository extends JpaRepository<BoardReply, Long> {
      * 작성자 id로 댓글 조회
      */
     List<BoardReply> findById(Member member);
+    
+    /**
+     * @author bhy98 백혜윤
+     * 작성자 id로 댓글 삭제
+     */
+    void deletaAllById(Member member);
 }
